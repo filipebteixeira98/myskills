8f23b676e0a0f7993f6b68c585f1bdac3432b8fb
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2025-10-08 17:06:24 -0300

    refactor(home): page component styles to StyleSheet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,20 @@
 import React, { Fragment } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export function Home() {
   return (
     <Fragment>
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Hello, World!</Text>
+      <View style={styles.container}>
+        <Text>Welcome, John!</Text>
       </View>
-      <Text style={{ alignSelf: 'center', marginBottom: 20 }}>This is React Native</Text>
     </Fragment>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
