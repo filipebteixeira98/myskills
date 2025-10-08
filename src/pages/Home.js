897efa924369f14f194d4df387609114d6c1f25b
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2025-10-08 18:05:45 -0300

    feat(app): add functionality that allows users to add new skill

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput, Platform, TouchableOpacity } from 'react-native';
 
 export function Home() {
+  const [newSkill, setNewSkill] = useState('');
+
+  const [mySkills, setMySkills] = useState([]);
+
+  function handleAddNewSkill() {
+    setMySkills(previousState => [...previousState, newSkill]);
+  }
+
   return (
     <>
       <View style={styles.container}>
         <Text style={styles.title}>Welcome, John!</Text>
-        <TextInput style={styles.input} placeholder='New skill' placeholderTextColor="#555" />
-        <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <TextInput style={styles.input} placeholder='New skill' placeholderTextColor="#555" onChangeText={setNewSkill} />
+        <TouchableOpacity style={styles.button} activeOpacity={0.7} onPress={handleAddNewSkill}>
           <Text style={styles.buttonText}>Add</Text>
         </TouchableOpacity>
-        <Text style={[styles.title, { marginTop: 50 }]}>My Skills</Text>
+        <Text style={[styles.title, { marginVertical: 50 }]}>My Skills</Text>
+        {mySkills.map((skill) => (
+          <TouchableOpacity key={skill} style={styles.buttonSkill} activeOpacity={0.7}>
+            <Text style={styles.textSkill}>{skill}</Text>
+          </TouchableOpacity>
+        ))}
       </View>
     </>
   );
@@ -47,5 +60,17 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 17,
     fontWeight: 'bold',
-  }
+  },
+  buttonSkill: {
+    backgroundColor: '#1f1e25',
+    padding: 15,
+    borderRadius: 50,
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  textSkill: {
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
 });
