5ca89a356b3b1543d64254a270dfab922af33b27
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2025-10-09 17:40:42 -0300

    chore(home): add ScrollView component as wrapper to small skills list

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Platform } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Platform, ScrollView } from 'react-native';
 
 import { Button } from '../components/Button';
 import { SkillCard } from '../components/SkillCard';
@@ -20,9 +20,11 @@ export function Home() {
         <TextInput style={styles.input} placeholder='New skill' placeholderTextColor="#555" onChangeText={setNewSkill} />
         <Button onPress={handleAddNewSkill} />
         <Text style={[styles.title, { marginVertical: 50 }]}>My Skills</Text>
-        {mySkills.map((skill) => (
-          <SkillCard key={skill} skill={skill} />
-        ))}
+        <ScrollView showsVerticalScrollIndicator={false}>
+          {mySkills.map((skill) => (
+            <SkillCard key={skill} skill={skill} />
+          ))}
+        </ScrollView>
       </View>
     </>
   );
