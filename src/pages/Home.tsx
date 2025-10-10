f743c1039f5d3377649c8d1df15e634be2a67b00
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2025-10-10 01:26:09 -0300

    refactor(home): component to use TypeScript typing

---
+++ src/pages/Home.tsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react"
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  FlatList,
+} from "react-native"
+
+import { Button } from "../components/Button"
+import { SkillCard } from "../components/SkillCard"
+
+interface SkillProps {
+  id: string
+  name: string
+}
+
+export function Home() {
+  const [newSkill, setNewSkill] = useState("")
+
+  const [mySkills, setMySkills] = useState<SkillProps[]>([])
+
+  const [greeting, setGreeting] = useState("")
+
+  function handleAddNewSkill() {
+    const skill = {
+      id: String(new Date().getTime()),
+      name: newSkill,
+    }
+    setMySkills((previousState) => [...previousState, skill])
+  }
+
+  useEffect(() => {
+    const currentHour = new Date().getHours()
+
+    if (currentHour < 12) {
+      setGreeting("Good morning ☀️")
+    } else if (currentHour >= 12 && currentHour < 18) {
+      setGreeting("Good afternoon 🌤️")
+    } else {
+      setGreeting("Good night 🌙")
+    }
+  }, [])
+
+  return (
+    <>
+      <View style={styles.container}>
+        <Text style={styles.title}>Welcome, John!</Text>
+        <Text style={styles.greetings}>{greeting}</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="New skill"
+          placeholderTextColor="#555"
+          onChangeText={setNewSkill}
+        />
+        <Button onPress={handleAddNewSkill} />
+        <Text style={[styles.title, { marginVertical: 50 }]}>My Skills</Text>
+        <FlatList
+          data={mySkills}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <SkillCard skill={item.name} />}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121015",
+    paddingVertical: 70,
+    paddingHorizontal: 30,
+  },
+  title: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  input: {
+    backgroundColor: "#1f1e25",
+    color: "#fff",
+    fontSize: 18,
+    padding: Platform.OS === "ios" ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  greetings: {
+    color: "#fff",
+    fontSize: 18,
+    marginTop: 5,
+  },
+})
